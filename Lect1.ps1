e329e7f70d28f160458670a9dd6c89d5150172ae
nadeempothiawala1987 <52731228+nadeempothiawala1987@users.noreply.github.com>
2021-05-26 01:31:40 +0530

    Add files via upload

---
+++ Lect1.ps1
@@ -0,0 +1,7 @@
+﻿#Connect-AzAccount
+$rg = 'armtemp2021'
+New-AzResourceGroup -Name $rg -Location westindia -Force
+New-AzResourceGroupDeployment -Name 'NewStorage' -ResourceGroupName $rg -TemplateFile .\orchestration.json
+
+#New-AzResourceGroupDeployment -Name 'NewStorage' -ResourceGroupName $rg -TemplateFile .\parafiles.json -TemplateParameterFile .\parafiles.parameters.json
+#New-AzResourceGroupDeployment -Name 'NewStorage' -ResourceGroupName $rg -TemplateFile '.\storage.json' -storagename 'strte2023' -storagesku 'Standard_LRS'
